cb962a0d9403913d0c4cc066c432479dc2868076
Sebastian <sebastianbecerra162004@gmail.com>
2026-06-09 07:09:33 -0400

    Inicializado el proyecto

---
+++ src/main/java/ve/student/netAnalyzer/NetAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ve.student.netAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ve/student/netAnalyzer/NetAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ve.student.netAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
